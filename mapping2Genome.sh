f7573fdb1b46490080d7b5863489bb4b4e21e474
jingkui <jingkui.wang@imp.ac.at>
2018-01-03 17:22:49 +0100

    add paired_end option for mapping2Genome.sh

--- mapping2Genome.sh
+++ mapping2Genome.sh
@@ -1,16 +1,20 @@
 ###########
 ## aim of script: map fastq (or fq) file to the selected gennome (mm9, mm10, ce11 or customized genomes already indexed) using bowtie2  
 ###########
-while getopts ":hg:" opts; do
+while getopts ":hg:p" opts; do
     case "$opts" in
         "h") 
 	    echo "script to map fastq or fq to the genome using bowite2"
-	    echo "Usage: $0 -g ce11 "
+	    echo "Usage: $0 -g ce11 (single_end)"
+	    echo "$0 -g cell -p (paired_end) "
 	    exit 0
 	    ;;
 	"g")
             genome="$OPTARG"
             ;;
+	"p")
+	    PAIRED="TRUE"
+	    ;;
         "?")
             echo "Unknown option $opts"
             ;;
@@ -30,6 +34,7 @@ case "$genome" in
 	echo "alignment to ce11 "
 	Genome="/groups/bell/jiwang/Genomes/C_elegans/ce11/ce11_index_4Bowtie/ce11"
 	;;
+    
     "mm9")
 	echo "align to mm9"
 	Genome="/groups/bell/jiwang/Genomes/Mouse/mm9_UCSC/mm9_index_4Bowtie/mm9"
@@ -42,6 +47,7 @@ case "$genome" in
 	echo "align to customized mm9_G11D"
 	Genome="/groups/bell/jiwang/Genomes/Mouse/mm9_UCSC_G11D_reproter/mm9_G11D_index/mm9_G11D"
 	;;
+    
     "hg19")
 	echo "align to hg19"
 	Genome="/groups/bell/jiwang/Genomes/Human/hg19/hg19_index_4Bowtie/hg19"
@@ -61,11 +67,30 @@ nb_cores=8
 mkdir -p $DIR_output
 mkdir -p $PWD/logs
 
-for file in ${DIR_input}/*.fastq;
-do
-    fname="$(basename $file)"
-    #echo $fname
-    fname=${fname%.fastq}
-    echo $fname
-    qsub -q public.q -o ${PWD}/logs -j yes -pe smp $nb_cores -cwd -b y -shell y -N $fname "module load samtools/0.1.18;module load bowtie2/2.2.4;bowtie2 -q -p $nb_cores -x $Genome -U $file | samtools view -bSu - | samtools sort - ${DIR_output}/$fname; samtools index ${DIR_output}/$fname.bam;" 
-done
+if [ "$PAIRED" != "TRUE" ]; then
+    echo "single-end fastq..."
+
+    for file in ${DIR_input}/*.fastq;
+    do
+	fname="$(basename $file)"
+        #echo $fname
+	fname=${fname%.fastq}
+	echo $fname
+	qsub -q public.q -o ${PWD}/logs -j yes -pe smp $nb_cores -cwd -b y -shell y -N bowtie2Map "module load samtools/0.1.18;module load bowtie2/2.2.4;bowtie2 -q -p $nb_cores -x $Genome -U $file | samtools view -bSu - | samtools sort - ${DIR_output}/$fname; samtools index ${DIR_output}/$fname.bam;" 
+    done
+else
+    echo "paired_end fastq ..."
+    
+    for seq1 in `ls ${DIR_input}/*.fastq |grep R1 `;
+    do
+	fname="$(basename $seq1)"
+        SUFFIX=${fname#*_R1}
+	fname=${fname%_R1*}
+	echo $SUFFIX;
+	echo $fname;
+	seq2=${DIR_input}/${fname}_R2${SUFFIX};
+	echo $seq1 
+	echo $seq2
+	qsub -q public.q -o ${PWD}/logs -j yes -pe smp $nb_cores -cwd -b y -shell y -N bowtie2Map "module load samtools/0.1.18; module load bowtie2/2.2.4; bowtie2 -q -p $nb_cores --no-mixed -X 2000 -x $Genome -1 $seq1 -2 $seq2  | samtools view -bSu - | samtools sort - ${DIR_output}/$fname; samtools index ${DIR_output}/$fname.bam;" 
+    done
+fi;
