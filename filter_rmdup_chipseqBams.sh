e6698863a6326dbbc8794c950a17ee020f53be62
jingkui.wang <jingkui.wang@imp.ac.at>
2019-12-12 12:10:54 +0100

    update filter_rmdup_chipseqBams.sh for cbe

--- filter_rmdup_chipseqBams.sh
+++ filter_rmdup_chipseqBams.sh
@@ -33,18 +33,24 @@ DIR_input="${PWD}/alignments/BAMs_All"
 DIR_uniq="${PWD}/alignments/BAMs_unique"
 DIR_uniq_rmdup="${PWD}/alignments/BAMs_unique_rmdup"
 DIR_stat="${PWD}/QCs/ALL/BamStat"
+DIR_logs=${PWD}/logs
 
-mkdir -p ${PWD}/logs
+mkdir -p $DIR_logs
 mkdir -p $DIR_uniq
 mkdir -p $DIR_uniq_rmdup
 mkdir -p $DIR_stat
 mkdir -p $DIR_stat/logs
 
+jobName='bamFiltering'
+#picardPath=$EBROOTPICARD
+echo $picardPath
 for file in ${DIR_input}/*.bam;
 do
     echo $file
     ff="$(basename $file)"
     ff="${ff%.bam}"
+    fname=$ff;
+    
     #newb="${newb%.bam}_filter"
     #echo $newb
     newb=$DIR_uniq/${ff}_uniq
@@ -54,40 +60,58 @@ do
     #echo $newbb 
     #echo $stat
     picardDup_QC=${DIR_stat}/${ff}_picardDup.qc.txt
-    
-    echo "module load samtools/0.1.18; 
-module load bedtools/2.25.0; 
-module load oracle-jdk/1.8.0_72
-module load picard-tools/2.6.0;
+
+    # creat the script for each sample
+    script=$DIR_logs/${fname}_${jobName}.sh
+    cat <<EOF > $script
+#!/usr/bin/bash
+
+#SBATCH --cpus-per-task=$nb_cores
+#SBATCH --time=120
+#SBATCH --mem=8000
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH -o $DIR_logs/${fname}.out
+#SBATCH -e $DIR_logs/${fname}.err
+#SBATCH --job-name $jobName
+
+module load samtools/0.1.20-foss-2018b
+module load bedtools/2.25.0-foss-2018b
+#module load oracle-jdk/1.8.0_72
+module load picard/2.18.27-java-1.8
 
 ## filter low quality reads 
 if [ ! -e $newb.bam ]; then
-if [ $PAIRED != \"TRUE\" ]; then 
-samtools view -q $MAPQ_cutoff -b $file | samtools sort - $newb;
-else
- samtools view -F 1804 -q $MAPQ_cutoff -b $file | samtools sort - $newb;
-fi  
-fi 
+   if [ $PAIRED != "TRUE" ]; then
+      samtools view -q $MAPQ_cutoff -b $file | samtools sort - $newb;
+   else
+	samtools view -F 1804 -q $MAPQ_cutoff -b $file | samtools sort - $newb;
+   fi  
+fi; 
+
 if [ ! -e $newb.bam.bai ]; then 
-samtools index $newb.bam; 
+   samtools index $newb.bam; 
 fi;
 
 ## remove duplicates 
 if [ ! -e $newbb.bam ]; then 
-#samtools rmdup -s $newb.bam $newbb.bam;
-java -jar /biosw/debian7-x86_64/picard-tools/2.6.0/picard.jar MarkDuplicates INPUT=$newb.bam OUTPUT=$newbb.bam METRICS_FILE=$picardDup_QC ASSUME_SORTED=true REMOVE_DUPLICATES=true
-fi; 
+   #samtools rmdup -s $newb.bam $newbb.bam;
+   java -jar \$EBROOTPICARD/picard.jar MarkDuplicates INPUT=$newb.bam OUTPUT=$newbb.bam METRICS_FILE=$picardDup_QC ASSUME_SORTED=true REMOVE_DUPLICATES=true
+fi;
+ 
 if [ ! -e $newbb.bam.bai ]; then 
-samtools index $newbb.bam; fi;
+   samtools index $newbb.bam; 
+fi;
 
 ## save statistical number for each bam 
 total=\$(samtools view -c $file); 
 mapped=\$(samtools view -c -F 4 $file); 
 unique=\$(samtools view -c $newb.bam); 
 rmdup=\$(samtools view -c $newbb.bam); 
-echo \"$ff \$total \$mapped \$unique \$rmdup\"|tr ' ' '\t' > $stat" > ${DIR_stat}/logs/${ff}_bamstat_2cluster.sh
-    
-    qsub -q public.q -o ${PWD}/logs -j yes -pe smp $nb_cores -cwd -b y -shell y -N bamStat "bash ${DIR_stat}/logs/${ff}_bamstat_2cluster.sh "
-    
+echo "\$ff \$total \$mapped \$unique \$rmdup\"|tr ' ' '\t' > $stat 
+
+EOF
+    cat $script
+    sbatch $script
     #break; 
 done
